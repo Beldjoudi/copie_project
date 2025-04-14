9e17d9d1f0efd31c64f9469a87233dc3be44751f
ThinkPad <mbeldjoudi3945@gmail.com>
2025-04-14 11:54:11 -0400

    envoi base de donnees

--- src/dao/DatabaseConnection.java
+++ src/dao/DatabaseConnection.java
@@ -13,7 +13,7 @@ public class DatabaseConnection {
     private static final String USER = "root";
 
     // Mot de passe de l'utilisateur MySQL
-    private static final String PASSWORD = "admin";
+    private static final String PASSWORD = "root";
 
     // Methode statique pour obtenir une connexion à la base
     public static Connection getConnection() throws SQLException {
